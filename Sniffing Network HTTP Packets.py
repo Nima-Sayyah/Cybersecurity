9f2e50e7e61820fb49fec008186bd325ab1039fe
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-02 23:55:08 +0000

    Update 1 - SNHP test

--- Sniffing Network HTTP Packets.py
+++ Sniffing Network HTTP Packets.py
@@ -7,3 +7,16 @@ init()
 GREEN = Fore.GREEN
 RED   = Fore.RED
 RESET = Fore.RESET
+
+def sniff_packets(iface=None):
+    """
+    Sniff 80 port packets with `iface`, if None (default), then the
+    Scapy's default interface is used
+    """
+    if iface:
+        # port 80 for http (generally)
+        # `process_packet` is the callback
+        sniff(filter="port 80", prn=process_packet, iface=iface, store=False)
+    else:
+        # sniff with default interface
+        sniff(filter="port 80", prn=process_packet, store=False)
