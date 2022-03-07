2e980aa276b85efcf978299208802981c7f3a1f7
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-07 23:59:39 +0000

    Update 3 - test

--- Scapy Essentials.py
+++ Scapy Essentials.py
@@ -4,4 +4,7 @@ from scapy.all import *
 
 #send(IP(src="10.1.99.100", dst="10.0.0.1")/ICMP()/"HelloWorld")
 
-send(IP(src="10.1.99.100", dst="10.0.0.1", ttl=128)/ICMP(type=0)/"HelloWorld")
+#send(IP(src="10.1.99.100", dst="10.0.0.1", ttl=128)/ICMP(type=0)/"HelloWorld")
+
+h=sr1(IP(dst="10.1.99.2")/ICMP())
+h.show()
