e42ed62cc2e2b585c151d3660c910a96f1bb62c2
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-11 23:13:30 +0000

    Update 15 - test

--- Scapy Essentials.py
+++ Scapy Essentials.py
@@ -13,22 +13,23 @@ from scapy.all import *
 #h.show()
 
 #p=sr(IP(dst="10.0.0.1")/TCP(dport=22))
-#p
-#p.show()
-#ans,unans=_
-#ans.summary()
+#p #(this only works using Scapy directly )
+#p.show() #(this only works using Scapy directly )
+#ans,unans=_ #(this only works using Scapy directly )
+#ans.summary() #(this only works using Scapy directly )
 
 
 #p=sr(IP(dst="10.0.0.1")/TCP(dport=[23,80,53]))
-#p
+#p 
 #ans,unans=_
 #ans.summary()
 
 
-p=sr(IP(dst="192.168.1.254")/TCP(sport=666,dport=[22,80,21,443], flags="S"))
+#p=sr(IP(dst="192.168.1.254")/TCP(sport=666,dport=[22,80,21,443], flags="S"))
 
 
 #p=sr(IP(dst="192.168.1.254")/TCP(sport=888,dport=[21,22,80,443], flags="A"))
 
 
-#p=sr(IP(src="10.1.99.100", dst="10.1.99.2")/TCP(sport=RandShort(), dport=[20,21,80,3389]),inter=0.5,retry=2,timeout=1)
+#p=sr(IP(src="10.1.99.100", dst="192.168.1.254")/TCP(sport=RandShort(), dport=[20,21,80,3389]),inter=0.5,retry=2,timeout=1)
+
