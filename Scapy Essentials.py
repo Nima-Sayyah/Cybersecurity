1fc0dca95612c3a8e198a329baf4525603b9b47b
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-24 23:08:11 +0000

    Update 44 - test

--- Scapy Essentials.py
+++ Scapy Essentials.py
@@ -94,8 +94,8 @@ from scapy.all import *
 
 #IP()/TCP()
 
-Ether()/IP()/TCP()
+#Ether()/IP()/TCP()
 
-IP()/TCP()/"GET / HTTP/1.0\r\n\r\n"
+#IP()/TCP()/"GET / HTTP/1.0\r\n\r\n"
 
-Ether()/IP()/IP()/UDP()
+#Ether()/IP()/IP()/UDP()
