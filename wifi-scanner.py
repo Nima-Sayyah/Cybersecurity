314ae9c640e43a90b68bb87ad86383728eaaebb1
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-29 23:51:12 +0100

    Update 1 - ws

--- wifi-scanner.py
+++ wifi-scanner.py
@@ -3,3 +3,7 @@ from threading import Thread
 import pandas
 import time
 import os
+
+
+networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
+networks.set_index("BSSID", inplace=True)
