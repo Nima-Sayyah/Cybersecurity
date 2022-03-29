6f4aaf30042c154769f713464642bd0db3e6dec0
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-29 23:51:59 +0100

    Update 2 - ws

--- wifi-scanner.py
+++ wifi-scanner.py
@@ -7,3 +7,8 @@ import os
 
 networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
 networks.set_index("BSSID", inplace=True)
+
+ef callback(packet):
+    if packet.haslayer(Dot11Beacon):
+        bssid = packet[Dot11].addr2
+        ssid = packet[Dot11Elt].info.decode()
