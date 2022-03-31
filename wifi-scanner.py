b5d969b63d048d7defd93b6dadee8c0d88907690
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-03-31 23:11:08 +0100

    Update 8 - ws

--- wifi-scanner.py
+++ wifi-scanner.py
@@ -4,7 +4,7 @@ import pandas
 import time
 import os
 
-# initialize the networks dataframe that will contain all access points nearby
+# initialize the networks dataframe containing all access points nearby
 networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
 # set the index BSSID (MAC address of the AP)
 networks.set_index("BSSID", inplace=True)
