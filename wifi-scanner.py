263bf1eb61f871da9591a2ed08c9c1b4b1e6ad80
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-01 23:58:17 +0100

    Update 15 - ws

--- wifi-scanner.py
+++ wifi-scanner.py
@@ -4,26 +4,26 @@ import pandas
 import time
 import os
 
-# initialize the networks dataframe that will contain all access points nearby
+# Initializing the networks dataframe containing all access points nearby
 networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# set the index BSSID (MAC address of the AP)
+# Setting the index BSSID (MAC address of the AP)
 networks.set_index("BSSID", inplace=True)
 
 def callback(packet):
     if packet.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
+        # Extracting the MAC address of the network
         bssid = packet[Dot11].addr2
-        # get the name of it
+        # Getting the name of it
         ssid = packet[Dot11Elt].info.decode()
         try:
             dbm_signal = packet.dBm_AntSignal
         except:
             dbm_signal = "N/A"
-        # extract network stats
+        # Extracting network stats
         stats = packet[Dot11Beacon].network_stats()
-        # get the channel of the AP
+        # Getting the channel of the AP
         channel = stats.get("channel")
-        # get the crypto
+        # Getting the crypto
         crypto = stats.get("crypto")
         networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
 
