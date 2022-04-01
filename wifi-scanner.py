16eb0e5a431af71f873ea4af059159c650ebb43b
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-01 23:49:18 +0100

    Update 10 - ws

--- wifi-scanner.py
+++ wifi-scanner.py
@@ -4,43 +4,25 @@ import pandas
 import time
 import os
 
-# Initializing the networks dataframe containing all access points nearby
+# initialize the networks dataframe that will contain all access points nearby
 networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# Setting the index BSSID (MAC address of the AP)
+# set the index BSSID (MAC address of the AP)
 networks.set_index("BSSID", inplace=True)
 
 def callback(packet):
     if packet.haslayer(Dot11Beacon):
-        # Extracting the MAC address of the network
+        # extract the MAC address of the network
         bssid = packet[Dot11].addr2
-        # Getting the name of it
+        # get the name of it
         ssid = packet[Dot11Elt].info.decode()
-
         try:
             dbm_signal = packet.dBm_AntSignal
         except:
             dbm_signal = "N/A"
-
-        # Extract network stats
+        # extract network stats
         stats = packet[Dot11Beacon].network_stats()
-        # Getting the channel of the AP
+        # get the channel of the AP
         channel = stats.get("channel")
-        # Getting the crypto
+        # get the crypto
         crypto = stats.get("crypto")
         networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
-
-def print_all():
-    while True:
-        os.system("clear")
-        print(networks)
-        time.sleep(0.5)
-
-if __name__ == "__main__":
-    # Interface name, check using iwconfig
-    interface = "wlan0mon"
-    # Starting the thread that prints all the networks
-    printer = Thread(target=print_all)
-    printer.daemon = True
-    printer.start()
-    # Starting sniffing
-    sniff(prn=callback, iface=interface)
