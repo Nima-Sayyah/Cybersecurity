a130fb29223525ad0ad4a674252e0fbb65366fb5
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-04 23:56:31 +0100

    Update 6 - NS

--- network-scanner.py
+++ network-scanner.py
@@ -11,14 +11,14 @@ ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether/arp
 result = srp(packet, timeout=3)[0]
 
-# a list of clients, we will fill this in the upcoming loop
+
 clients = []
 
 for sent, received in result:
-    # for each response, append ip and mac address to `clients` list
+
     clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-# print clients
+
 print("Available devices in the network:")
 print("IP" + " "*18+"MAC")
 
