4759937f8b1e7ec254a1920c195db737e9a5a834
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-05 23:12:58 +0100

    Update 2 - EDF

--- Encrypt-Decrypt-Files.py
+++ Encrypt-Decrypt-Files.py
@@ -7,3 +7,9 @@ def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
+
+def load_key():
+    """
+    Loads the key from the current directory named `key.key`
+    """
+    return open("key.key", "rb").read()
