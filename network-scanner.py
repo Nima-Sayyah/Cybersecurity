702da85c403be0fdf5c26a4273978bbeef6ef419
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-06 23:59:09 +0100

    Update 8 - NS

--- network-scanner.py
+++ network-scanner.py
@@ -1,12 +1,13 @@
 from scapy.all import ARP, Ether, srp
 
 target_ip = "192.168.1.1/24"
-
+# IP Address for the destination
+# create ARP packet
 arp = ARP(pdst=target_ip)
-
+# create the Ether broadcast packet
 
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-
+# stack them
 packet = ether/arp
 result = srp(packet, timeout=3)[0]
 
