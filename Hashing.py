755527f268ebaf074c4596de52421e7b1afbaa63
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-09 23:59:09 +0100

    Update 1 - H

--- Hashing.py
+++ Hashing.py
@@ -2,3 +2,6 @@ import hashlib
 
 # encode it to bytes using UTF-8 encoding
 message = "Some text to hash".encode()
+
+# hash with MD5 (not recommended)
+print("MD5:", hashlib.md5(message).hexdigest())
