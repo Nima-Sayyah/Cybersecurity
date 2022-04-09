fe450ec7c9e4222b2ca6ecce862f535ade806b2f
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-09 23:59:43 +0100

    Update 2 - H

--- Hashing.py
+++ Hashing.py
@@ -5,3 +5,8 @@ message = "Some text to hash".encode()
 
 # hash with MD5 (not recommended)
 print("MD5:", hashlib.md5(message).hexdigest())
+
+# hash with SHA-2 (SHA-256 & SHA-512)
+print("SHA-256:", hashlib.sha256(message).hexdigest())
+
+print("SHA-512:", hashlib.sha512(message).hexdigest())
