fbe6c06145e39edd06a14dd3544ed74b1310b27b
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-11 21:56:25 +0100

    Update 4 - H

--- Hashing.py
+++ Hashing.py
@@ -15,3 +15,9 @@ print("SHA-512:", hashlib.sha512(message).hexdigest())
 print("SHA-3-256:", hashlib.sha3_256(message).hexdigest())
 
 print("SHA-3-512:", hashlib.sha3_512(message).hexdigest())
+
+# hash with BLAKE2
+# 256-bit BLAKE2 (or BLAKE2s)
+print("BLAKE2c:", hashlib.blake2s(message).hexdigest())
+# 512-bit BLAKE2 (or BLAKE2b)
+print("BLAKE2b:", hashlib.blake2b(message).hexdigest())
