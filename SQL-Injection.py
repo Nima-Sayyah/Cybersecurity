fb3eb49528cc00a7f9bfe086a39e369c3015e64d
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-13 23:58:02 +0100

    Update 3 - SQ

--- SQL-Injection.py
+++ SQL-Injection.py
@@ -8,3 +8,8 @@ from pprint import pprint
 # initialize an HTTP session & set the browser
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
+
+def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
+    soup = bs(s.get(url).content, "html.parser")
+    return soup.find_all("form")
