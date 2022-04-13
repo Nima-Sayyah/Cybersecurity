14db93d4a1e813fc5f3b9092c31728d0f6d0235f
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-04-13 23:56:12 +0100

    Update 2 - SQ

--- SQL-Injection.py
+++ SQL-Injection.py
@@ -4,3 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 from pprint import pprint
+
+# initialize an HTTP session & set the browser
+s = requests.Session()
+s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
