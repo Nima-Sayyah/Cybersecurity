2bf4e357a9987154050ac63f9ef15fd92657719d
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-05-16 23:24:54 +0100

    Update 4 - SA

--- shodan-api.py
+++ shodan-api.py
@@ -17,3 +17,9 @@ def request_page_from_shodan(query, page=1):
         except shodan.APIError as e:
             print(f"Error: {e}")
             time.sleep(5)
+
+# Try the default credentials on a given instance of DVWA, simulating a real user trying the credentials
+# visits the login.php page to get the CSRF token, and tries to login with admin:password
+def has_valid_credentials(instance):
+    sess = requests.Session()
+    proto = ('ssl' in instance) and 'https' or 'http'
