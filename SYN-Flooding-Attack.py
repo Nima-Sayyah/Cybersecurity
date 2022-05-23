7ad03735f14802afab697243cbb4675c2198c75a
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-05-23 23:19:36 +0100

    Update 1 - SFA

--- SYN-Flooding-Attack.py
+++ SYN-Flooding-Attack.py
@@ -1,2 +1,7 @@
 # pip3 install scapy
 from scapy.all import *
+
+# target IP address (should be a testing router/firewall)
+target_ip = "192.168.1.1"
+# the target port u want to flood
+target_port = 80
