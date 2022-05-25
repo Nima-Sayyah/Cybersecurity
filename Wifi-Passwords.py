a2a644b804a29ddcea238c42686d1318a0d307b9
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-05-25 23:56:14 +0100

    Update 2 - WP

--- Wifi-Passwords.py
+++ Wifi-Passwords.py
@@ -3,3 +3,12 @@ import os
 import re
 from collections import namedtuple
 import configparser
+
+def get_linux_saved_wifi_passwords(verbose=1):
+    """Extracts saved Wi-Fi passwords saved in a Linux machine, this function extracts data in the
+    `/etc/NetworkManager/system-connections/` directory
+    Args:
+        verbose (int, optional): whether to print saved profiles real-time. Defaults to 1.
+    Returns:
+        [list]: list of extracted profiles, a profile has the fields ["ssid", "auth-alg", "key-mgmt", "psk"]
+    """
