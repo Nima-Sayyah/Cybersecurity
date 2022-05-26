93f29b8a36de23920e18c5a90ee60bf9f5f2edc4
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-05-26 23:56:19 +0100

    Update 4 - WP

--- Wifi-Passwords.py
+++ Wifi-Passwords.py
@@ -15,3 +15,8 @@ def get_linux_saved_wifi_passwords(verbose=1):
     network_connections_path = "/etc/NetworkManager/system-connections/"
     fields = ["ssid", "auth-alg", "key-mgmt", "psk"]
     Profile = namedtuple("Profile", [f.replace("-", "_") for f in fields])
+    profiles = []
+    for file in os.listdir(network_connections_path):
+        data = { k.replace("-", "_"): None for k in fields }
+        config = configparser.ConfigParser()
+        config.read(os.path.join(network_connections_path, file))
