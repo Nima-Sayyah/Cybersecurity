c3931acfb941d986f73f134843dd363b54a4d9cb
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-22 23:59:59 +0100

    Update 3 - SS

--- subdomain-scannar.py
+++ subdomain-scannar.py
@@ -17,3 +17,10 @@ discovered_subdomains = []
 for subdomain in subdomains:
     # construct the url
     url = f"http://{subdomain}.{domain}"
+
+try:
+        # if this raises an ERROR, that means the subdomain does not exist
+        requests.get(url)
+    except requests.ConnectionError:
+        # if the subdomain does not exist, just pass, print nothing
+        pass
