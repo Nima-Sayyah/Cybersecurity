98980d2ed3e2af3c3092482c135aa7dd9ad6ec92
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-22 23:58:42 +0100

    Update 1 - SS

--- subdomain-scannar.py
+++ subdomain-scannar.py
@@ -4,3 +4,10 @@ import requests
 
 # the domain to scan for subdomains
 domain = "google.com"
+
+# read all subdomains
+file = open("subdomains.txt")
+# read all content
+content = file.read()
+# split by new lines
+subdomains = content.splitlines()
