d843648bede30d143ba521243c71ab0bd30fbf2b
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-25 23:55:42 +0100

    Update 6 - SS

--- subdomain-scannar.py
+++ subdomain-scannar.py
@@ -7,21 +7,28 @@ domain = "google.com"
 
 # read all subdomains
 file = open("subdomains.txt")
+
 # read all content
 content = file.read()
+
 # split by new lines
 subdomains = content.splitlines()
 
 # a list of discovered subdomains
 discovered_subdomains = []
+
 for subdomain in subdomains:
+
     # construct the url
     url = f"http://{subdomain}.{domain}"
 
     try:
+
         # if this raises an ERROR, that means the subdomain does not exist
         requests.get(url)
+
     except requests.ConnectionError:
+
         # if the subdomain does not exist, just pass, print nothing
         pass
 
