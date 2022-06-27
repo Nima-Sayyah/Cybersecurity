32d282611527bfa946dd2443b9931e35083b36ab
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-27 23:42:11 +0100

    Update 2 - CP

--- crack-pdfs.py
+++ crack-pdfs.py
@@ -3,3 +3,13 @@ from tqdm import tqdm
 
 # load password list
 passwords = [ line.strip() for line in open("wordlist.txt") ]
+
+try:
+    # open PDF file
+    with pikepdf.open("foo-protected.pdf", password=password) as pdf:
+            # Password decrypted successfully, break out of the loop
+            print("[+] Password found:", password)
+            break
+    except pikepdf._qpdf.PasswordError as e:
+        # wrong password, just continue in the loop
+        continue
