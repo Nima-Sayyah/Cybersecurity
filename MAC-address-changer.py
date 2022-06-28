48c00dac5906937f4ddd4a36a30169a8526be706
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-06-28 23:57:44 +0100

    Update 1 - MAC

--- MAC-address-changer.py
+++ MAC-address-changer.py
@@ -2,3 +2,7 @@ import subprocess
 import string
 import random
 import re
+
+"""Generate and return a MAC address in the format of Linux"""
+    # get the hexdigits uppercased
+    uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
