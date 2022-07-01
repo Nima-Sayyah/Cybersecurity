76f56170cc477112025f7cfb75b9e06a47fd2ab1
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-07-01 23:20:21 +0100

    Update 11 - MAC

--- MAC-address-changer.py
+++ MAC-address-changer.py
@@ -51,3 +51,6 @@ if __name__ == "__main__":
     print("[*] Old MAC address:", old_mac_address)
     # change the MAC address
     change_mac_address(iface, new_mac_address)
+    # check if it's really changed
+    new_mac_address = get_current_mac_address(iface)
+    print("[+] New MAC address:", new_mac_address)
