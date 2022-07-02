34a38c276ae61f3ad22be15fe18e97806cfbdd3d
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-07-03 00:00:04 +0100

    Update 12 - MAC

--- MAC-address-changer.py
+++ MAC-address-changer.py
@@ -4,9 +4,12 @@ import random
 import re
 
 def get_random_mac_address():
+
 """Generate and return a MAC address in the format of Linux"""
+
     # get the hexdigits uppercased
     uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
+
     # 2nd character must be 0, 2, 4, 6, 8, A, C, or E
     mac = ""
     for i in range(6):
@@ -19,15 +22,19 @@ def get_random_mac_address():
     return mac.strip(":")
 
 def get_current_mac_address(iface):
+
     # use the ifconfig command to get the interface details, including the MAC address
     output = subprocess.check_output(f"ifconfig {iface}", shell=True).decode()
     return re.search("ether (.+) ", output).group().split()[1].strip()
 
 def change_mac_address(iface, new_mac_address):
+
     # disable the network interface
     subprocess.check_output(f"ifconfig {iface} down", shell=True)
+
     # change the MAC
     subprocess.check_output(f"ifconfig {iface} hw ether {new_mac_address}", shell=True)
+
     # enable the network interface again
     subprocess.check_output(f"ifconfig {iface} up", shell=True)
 
